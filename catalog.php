5f22f0c0cd4130e9028c21b153b45ed512d82e1c
Andrew <prishepa.andrew@mail.ru>
2023-08-04 13:28:51 +0300

    commit

--- catalog.php
+++ catalog.php
@@ -9,6 +9,14 @@
     <title>Document</title>
 </head>
 <body>
+<header>
+        <div class="title">MyApp</div>
+        <ul class="menu">
+            <li><a href="index.html" class="active">Главная</a></li>
+            <li><a href="catalog.php">Каталог</a></li>
+            <li><a href="#">Вход</a></li>
+        </ul>
+    </header>
     123321
 </body>
 </html>
